22e90bbff17ae56a47202c08933cba4b4d8205d8
DaviAMSilva <daviamsilva@hotmail.com>
2021-10-22 17:59:26 -0300

    Melhorado usando Github Copilot

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import argparse
-import os
 from pathlib import Path
 
 import mistune
-
 
 # Lê todos os arquivos markdown dentro de input_dir
 # Depois disso converte os arquivos markdown para html e salva
@@ -11,42 +9,25 @@ import mistune
 # relativa de pastas encontrada em input_dir
 # Ou seja: input_dir/a/b/file.md -> output_dir/a/b/file.html
 def main(input_dir, output_dir):
-    # Acha todos os arquivos markdown em input_dir recusivamente
-    md_list = Path(f"{input_dir}").glob("**/*.md")
+    """For each markdown file in input_dir convert it to html in output_dir and print the file name if the markdown file is more recent than the html"""
+    for markdown_file in Path(input_dir).glob('**/*.md'):
+        html_file = Path(output_dir, markdown_file.relative_to(input_dir))
+        html_file.parent.mkdir(parents=True, exist_ok=True)
+        if not html_file.exists() or markdown_file.stat().st_mtime > html_file.stat().st_mtime:
+            print("Converted: " + markdown_file.name)
+            with open(markdown_file, 'r', encoding='utf-8') as file:
+                content = file.read()
+            html = mistune.markdown(content)
+            with open(html_file, 'w', encoding='utf-8') as file:
+                file.write(html)
 
-    for md_path in md_list:
-        # Caminho relativo a partir de input_dir das páginas
-        # dir = pasta, path = arquivo
-        md_relative_dir = str(Path(f"{os.path.dirname(md_path)}")).replace(
-            input_dir + "/", "")
-        md_relative_path = str(Path(
-            md_relative_dir + "/" + os.path.basename(md_path))).replace(input_dir + "/", "")
-
-        print("Arquivo convertido:", md_relative_path)
-
-        # Cria a pasta se ela não existe
-        if not os.path.exists(str(Path(f"{output_dir}/{md_relative_dir}"))):
-            os.makedirs(str(Path(f"{output_dir}/{md_relative_dir}")))
-
-        # Lê o arquivo markdown
-        with open(str(Path(f"{input_dir}/{md_relative_path}")), 'r', encoding="utf8") as md_file:
-            text = md_file.read()
-
-        # Faz a conversão
-        html = mistune.html(text)
-
-        # Salva o arquivo na pasta output_dir
-        with open(str(Path(f"{output_dir}/{md_relative_path.replace('.md', '.html')}")), 'w', encoding="utf8") as html_file:
-            html_file.write(html)
 
 
 if __name__ == "__main__":
-    # Colects the arguments input_dir e output_dir
-    PARSER = argparse.ArgumentParser(description='Converter Markdown para HTML')
-    PARSER.add_argument("input_dir", help="Caminho para ser convertido")
-    PARSER.add_argument("output_dir", help="Caminho para salvar")
-    ARGS = PARSER.parse_args()
-
-    INPUT_DIR, OUTPUT_DIR = ARGS.input_dir, ARGS.output_dir
-
-    main(INPUT_DIR, OUTPUT_DIR)
+    """Collects the input_dir and output_dir from command line"""
+    parser = argparse.ArgumentParser(description='Convert markdown files to html')
+    parser.add_argument('input_dir', help='the directory containing the markdown files')
+    parser.add_argument('output_dir', help='the directory to save the html files')
+    args = parser.parse_args()
+    main(args.input_dir, args.output_dir)
+    print('Converted all markdown files to html')
